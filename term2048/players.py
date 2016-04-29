bf8f7b99806067dbdaffc0510fb96b00daec0f5b
Kristian Järvenpää <kristian.jarvenpaa@gmail.com>
2016-04-29 11:33:18 +0200

    Implemented q-learing agent and moved all AI-players to player.py

--- term2048/players.py
+++ term2048/players.py
@@ -1,15 +1,75 @@
-# Create AI in this file, so we do not mess with ai.py
-
-import random
 from board import Board
+import random
 
-def biasRandom_ai(board, score):
-    r = random.random()
-    if r < 0.6:
-        return Board.UP
-    elif r < 0.61:
-        return Board.DOWN
-    elif r < 0.90:
-        return Board.LEFT
-    else:
-        return Board.RIGHT
+class AI:
+    
+    def __init__(self,size):
+        self.prev_state = str((size*size)*[0]) 
+        self.prev_score = 0  
+        self.prev_move = 0
+        self.states = {}
+        self.states[self.prev_state] = self.init_state(0,0.2)
+    
+    def init_state(self,a,b):
+        return [random.uniform(a,b),random.uniform(a,b),random.uniform(a,b),random.uniform(a,b)]
+
+    def get_state(self,board):
+        """
+        Append all rows to one array, starting from the uppermost one.
+        """
+        s = range(board.size())
+        return [ board.getCell(x,y) for y in s for x in s]
+    
+    def largest_tile(self, board):
+        """ Returns the value of the largest tile at board """
+        return max(get_state(board))
+    
+    def random_ai(self,board, score):
+        moves = [Board.UP, Board.DOWN, Board.LEFT, Board.RIGHT]
+        r = random.randint(0,3)
+        #time.sleep(0.2)
+        return moves[r]
+
+    def biasRandom_ai(self,board, score):
+        r = random.random()
+        if r < 0.6:
+            return Board.UP
+        elif r < 0.61:
+            return Board.DOWN
+        elif r < 0.90:
+            return Board.LEFT
+        else:
+            return Board.RIGHT
+
+    def q_learning_ai(self,board, score):
+        new_state = str(self.get_state(board))
+        r = score - self.prev_score
+
+        gamma = 1 #0.85 
+        alpha = 0.005
+        epsilon = 0.9
+
+        moves = [Board.UP, Board.DOWN, Board.LEFT, Board.RIGHT]
+        move = 0
+        if new_state in self.states:
+            if random.uniform(0,1) < epsilon:
+                move = self.states[new_state].index(max(self.states[new_state]))
+            else:
+                move = moves[random.randint(0,3)]
+        else:
+            move = moves[random.randint(0,3)]
+            self.states[new_state] = self.init_state(0,0.2)
+
+        # This is the update rule. ( minus 1 from self.prev_move to correct indexing) 
+        best_move = max(self.states[new_state]) 
+        self.states[self.prev_state][self.prev_move-1] += alpha*(r + gamma*best_move - self.states[self.prev_state][self.prev_move-1])
+
+        self.prev_state = new_state
+        self.prev_move = move
+        self.prev_score = score
+        return move
+    
+    def print_states(self):
+        for key in self.states:
+            print(key, self.states[key])
+
